67a8d94dfb6a803222079e26189e93d3bb708be5
Keksovmen <Keksovmen@yandex.ru>
2021-10-08 15:26:24 +0300

    Made user_id as explicit for instance methods, and fixed bug when deleting and redirecting

--- com/keksovmen/Controllers/DirectoryController.py
+++ com/keksovmen/Controllers/DirectoryController.py
@@ -16,16 +16,9 @@ class DirectoryController(AbstractController):
 
 	@expose("com/keksovmen/Controllers/xhtml/dir/directoryView.xhtml")
 	def view(self, dir_id: int = 0):
-
 		current_dir = ModelInit.session.query(Directory) \
 			.filter(Directory.creator == session.get('u_id', None)) \
 			.filter(Directory.dir_id == dir_id).first()
-		# par = current_dir.parrent
-		# cards = ModelInit.session.query(Card) \
-		# 	.filter(Card.creator == session.get('u_id', None)) \
-		# 	.filter(Card.dir_id == dir_id) \
-		# 	.order_by(Card.modification_time.desc())
-		# parent = Directory.getDirectory(dir_id, session.get('u_id', None))
 		return dict(current_dir=current_dir)
 
 	@expose("com/keksovmen/Controllers/xhtml/dir/dir.xhtml")
@@ -33,7 +26,8 @@ class DirectoryController(AbstractController):
 		result = super(DirectoryController, self).create(
 			parent_id=int(parent_id),
 			title=title,
-			description=description)
+			description=description,
+			user_id=session.get('u_id', None))
 		if "form" in result.keys():
 			return result
 		currDir = result["model"]
@@ -45,7 +39,8 @@ class DirectoryController(AbstractController):
 		result = super(DirectoryController, self).edit(
 			dir_id=dir_id,
 			title=title,
-			description=description)
+			description=description,
+			user_id=session.get('u_id', None))
 		if "form" in result.keys():
 			return result
 		currDir = result["model"]
@@ -55,12 +50,12 @@ class DirectoryController(AbstractController):
 
 	@expose("com/keksovmen/Controllers/xhtml/dir/dir.xhtml")
 	def delete(self, dir_id: int):
-		result = super(DirectoryController, self).delete(dir_id=dir_id)
+		result = super(DirectoryController, self) \
+			.delete(dir_id=dir_id, user_id=session.get('u_id', None))
 		if "form" in result.keys():
 			return result
 		currDir = result["model"]
-		redirect("view?dir_id={}".format(
-			currDir.parent.dir_id if currDir.parent else 0))
+		redirect("view?dir_id={}".format(currDir.parent_id))
 
 	def _getDefaultForm(self, **kwargs):
 		form = Form()
@@ -73,10 +68,11 @@ class DirectoryController(AbstractController):
 		form.addField(FormField("pageTitle"))
 		form.addField(FormField("button"))
 		form.addField(FormField("action"))
+		form.addField(FormField("action"))
 		form.setValues(**kwargs)
 		return form
 
-	def _getCreateForm(self, parent_id, title, description) -> Form:
+	def _getCreateForm(self, parent_id, title, description, user_id) -> Form:
 		form = self._getDefaultForm(title=title,
 									description=description,
 									parent_id=parent_id,
@@ -84,14 +80,15 @@ class DirectoryController(AbstractController):
 									action="create",
 									button="Create")
 		form.title.addCheckCondition(
-			lambda v: Directory.isNameFree(title, parent_id, session['u_id']),
+			lambda v: Directory.isNameFree(title, parent_id, user_id),
 			"Such title already exists in current directory")
 		return form
 
 	def _getEditForm(self, model: Directory,
 					 title,
 					 description,
-					 dir_id) -> Form:
+					 dir_id,
+					 **kwargs) -> Form:
 		form = self._getDefaultForm(title=title,
 									description=description,
 									dir_id=dir_id,
@@ -104,26 +101,26 @@ class DirectoryController(AbstractController):
 			"Such title already exists in current directory")
 		return form
 
-	def _getDeleteForm(self, dir: Directory) -> Form:
-		form = self._getDefaultForm(title=dir.title,
-									description=dir.description,
-									dir_id=dir.dir_id,
-									parent_id=dir.parent_id,
+	def _getDeleteForm(self, directory: Directory) -> Form:
+		form = self._getDefaultForm(title=directory.title,
+									description=directory.description,
+									dir_id=directory.dir_id,
+									parent_id=directory.parent_id,
 									pageTitle="Delete directory",
 									action="delete",
 									button="Delete")
-		form.addField(FormField("dir", dir))
+		form.addField(FormField("dir", directory))
 		return form
 
-	def _createModelObject(self, parent_id, title, description):
+	def _createModelObject(self, parent_id, title, description, user_id):
 		return Directory(title=title,
 						 description=description,
-						 creator=session['u_id'],
-						 dir_id=User.generateDirectoryId(session['u_id']),
+						 creator=user_id,
+						 dir_id=User.generateDirectoryId(user_id),
 						 parent_id=parent_id)
 
-	def _getModelObject(self, dir_id, **kwargs) -> Directory:
-		return Directory.getDirectory(dir_id, session["u_id"])
+	def _getModelObject(self, dir_id, user_id, **kwargs) -> Directory:
+		return Directory.getDirectory(dir_id, user_id)
 
 	def _updateFieldsOnGetEdit(self, model: Directory, kwargs: dict):
 		kwargs['title'] = model.title
